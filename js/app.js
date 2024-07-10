eda4f94a329208220e38aa77afd6d5910702ef73
farhutdinoff <33476367+farhutdinoff@users.noreply.github.com>
2024-07-10 08:53:48 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,3 +2,7 @@ $('#menuToggle').click( function(){
     $('.header-nav').toggleClass("active");
     $('body').toggleClass("overflow-hidden");
   });
+
+  $('.product-box__btns button').click( function(){
+    $(this).toggleClass("active");
+  });
